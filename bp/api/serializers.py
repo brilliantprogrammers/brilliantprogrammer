0b9973abba051a58866f173c10eeb3c4df43bd91
Brillianttyagi <deepanshubhai84@gmail.com>
2021-08-26 16:44:49 +0530

    update serializers

--- bp/api/serializers.py
+++ bp/api/serializers.py
@@ -5,4 +5,4 @@ from bp.models import Blog
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
-        fields = ['id','title','body','image','author','slug','hit_count_generic']
+        fields = ['id','title','body','image','author','slug']
